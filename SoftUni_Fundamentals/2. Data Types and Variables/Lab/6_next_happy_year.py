9833dfb6dfef173c67c445a08dfeabc9dba89415
Nikolay Georgiev <122815487+nikichhh@users.noreply.github.com>
2023-03-22 21:54:55 +0200

    Add files via upload

---
+++ SoftUni_Fundamentals/2. Data Types and Variables/Lab/6_next_happy_year.py
@@ -0,0 +1,30 @@
+# input_year = int(input())
+# next_year = input_year + 1
+# next_happy_year = ""
+# brake_first_loop = False
+# while True:
+#     str_next_year = str(next_year)
+#     next_happy_year = str_next_year[0]
+#     for i in range(1, len(str_next_year)):
+#         for j in range(0, i):
+#             if str_next_year[i] == next_happy_year[j]:
+#                 brake_first_loop = True
+#                 break
+#         if brake_first_loop:
+#             break
+#         else:
+#             next_happy_year = next_happy_year + str_next_year[i]
+#     if len(next_happy_year) == len(str_next_year):
+#         break
+#     next_happy_year = ""
+#     brake_first_loop = False
+#     next_year += 1
+# print(next_happy_year)
+
+year = int(input())
+year_as_string = str(year)
+
+while len(year_as_string) != len(set(year_as_string)):
+    year += 1
+    year_as_string = str(year)
+print(year)
