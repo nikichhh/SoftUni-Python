157e8c27a1b390303c08bc071c2a70cb5934502d
Nikolay Georgiev <122815487+nikichhh@users.noreply.github.com>
2023-03-22 21:27:27 +0200

    Delete 3_conditional_statements directory

--- 3_conditional_statements/4ta zadacha.py
+++
@@ -1,5 +0,0 @@
-a = input()
-if a == "s3cr3t!P@ssw0rd":
-    print("Welcome")
-else:
-    print("Wrong password!")
--- 3_conditional_statements/5ta zadacha.py
+++
@@ -1,8 +0,0 @@
-a = int(input())
-
-if a<100:
-    print("Less than 100")
-elif a > 200:
-    print("Greater than 200")
-else:
-    print("Between 100 and 200")
