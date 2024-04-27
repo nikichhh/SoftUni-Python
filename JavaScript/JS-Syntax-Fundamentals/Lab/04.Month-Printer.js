40016f9f0a1e4ac732079d40ae07fc45d421f9d5
Nikolay Georgiev <nikstershmit@gmail.com>
2024-04-27 15:16:11 +0300

    JS Tasks

---
+++ JavaScript/JS-Syntax-Fundamentals/Lab/04.Month-Printer.js
@@ -0,0 +1,73 @@
+function month_printer(month_num) {
+    switch (month_num) {
+        case 1:
+            console.log("January");
+            break;
+        case 2:
+            console.log("February");
+            break;
+        case 3:
+            console.log("March");
+            break;
+        case 4:
+            console.log("April");
+            break;
+        case 5:
+            console.log("May");
+            break;
+        case 6:
+            console.log("June");
+            break;
+        case 7:
+            console.log("July");
+            break;
+        case 8:
+            console.log("August");
+            break;
+        case 9:
+            console.log("September");
+            break;
+        case 10:
+            console.log("October");
+            break;
+        case 11:
+            console.log("November");
+            break;
+        case 12:
+            console.log("December");
+            break;
+        default:
+            console.log("Error!");
+            break;
+    }
+
+    // 2nd solution
+
+    // if (month_num == 1) {
+    //     console.log("January");
+    // } else if (month_num == 2) {
+    //     console.log("February");
+    // } else if (month_num == 3) {
+    //     console.log("March");
+    // } else if (month_num == 4) {
+    //     console.log("April");
+    // } else if (month_num == 5) {
+    //     console.log("May");
+    // } else if (month_num == 6) {
+    //     console.log("June");
+    // } else if (month_num == 7) {
+    //     console.log("July");
+    // } else if (month_num == 8) {
+    //     console.log("August");
+    // } else if (month_num == 9) {
+    //     console.log("September");
+    // } else if (month_num == 10) {
+    //     console.log("October");
+    // } else if (month_num == 11) {
+    //     console.log("November");
+    // } else if (month_num == 12) {
+    //     console.log("December");
+    // } else {
+    //     console.log("Error!");
+    // }
+}
---
+++ JavaScript/JS-Syntax-Fundamentals/Lab/10.Numbers-From-M-To-N.js
@@ -0,0 +1,5 @@
+function print(m, n) {
+    for(let i = m; i >= n; i--) {
+        console.log(i);
+    }
+}
